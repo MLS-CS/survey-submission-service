afa025ad53c84a01613cf6ff4c3ba3230c8778c5
Anant Pawar <anant.c.pawar@gmail.com>
2019-09-08 22:57:11 +0530

    initalized project

---
+++ src/main/java/com/marketlogicsoftware/cs/surveysubmissionservice/SurveySubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marketlogicsoftware.cs.surveysubmissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveySubmissionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveySubmissionServiceApplication.class, args);
+	}
+
+}
